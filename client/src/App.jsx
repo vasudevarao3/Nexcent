fd4f369f58c0a7c6562c92f5d400c9ae27e6e38e
vasudevarao3 <kvasudevarao3@gmail.com>
2024-07-07 16:41:48 +0530

    Adding Icons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import About from './components/About'
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
