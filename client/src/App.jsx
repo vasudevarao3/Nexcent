d1755087e61e52cb155576b4d7f922d249c559be
vasudevarao3 <kvasudevarao3@gmail.com>
2024-07-07 18:49:10 +0530

    Business,  Experience

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from './components/Navbaar'
 import About from './components/About'
 import Home from './components/Home'
 import Clients from './components/Clients'
+import Experience from './components/Experience'
+import Business from './components/Business'
 
 export default function App() {
   return (
@@ -14,6 +16,8 @@ export default function App() {
       </Routes>
       <Home/>
       <Clients/>
+      <Experience/>
+      <Business/>
     </BrowserRouter>
   )
 }
---
+++ client/src/components/Experience.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Image from "../assets/Images/mobile1.svg"
+import { Button } from 'flowbite-react';
+
+export default function Experience() {
+  return (
+    <div>
+      <div className="flex flex-col md:flex-row itms-cente sm:px-[1rem] md:px-[5rem] p-5">
+        <div className="max-w-[70rem]">
+          <img src={Image} width={500} height={500} />
+        </div>
+        <div className="w-screen whitespace-pre-wrap md:pt-[0rem] lg:pt-[5rem] justify-center items-center text-left text-D_Grey font-Inter p-5 md:p-0">
+          <h1 className="text-D_Grey font-semibold text-3xl md:text-2xl pt-10 pb-2 md:pt-9 md:pb-3">
+            The unseen of spending three years at Pixelgrade
+          </h1>
+          <p className="text-[#717171] text-sm md:max-w-[35rem] ">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit
+            amet justo ipsum. Sed accumsan quam vitae est varius fringilla.
+            Pellentesque placerat vestibulum lorem sed porta. Nullam mattis
+            tristique iaculis. Nullam pulvinar sit amet risus pretium auctor.
+            Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec
+            elementum pulvinar odio.
+          </p>
+          <div className="pt-4 p-2 md:pt-8 md:p-0 ">
+            <Button color="green">
+                Learn More
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
